f802a3bf1c4e6b7af36aa7bc7cd82fa56e3df177
Jobin Mathew <jobin@bolder.no>
2019-07-27 16:16:34 +0530

    Links to home and results

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => (
-    <>
-
-        <section className="section parallax-container breadcrumbs-wrap">
-            <div className="material-parallax parallax"><img src="images/bg-breadcrumbs-1-1920x726.jpg" alt="images/bg-breadcrumbs-1-1920x726.jpg" /></div>
-            <div className="parallax-content breadcrumbs-custom context-dark">
-                <div className="container">
-                    <h3 className="breadcrumbs-custom-title">FUTSAL 2019</h3>
+    <section className="section parallax-container breadcrumbs-wrap">
+        <div className="material-parallax parallax"><img src="images/bg-breadcrumbs-1-1920x726.jpg" alt="images/bg-breadcrumbs-1-1920x726.jpg" /></div>
+        <div className="parallax-content breadcrumbs-custom context-dark">
+            <div className="container">
+                <h3 className="breadcrumbs-custom-title">FUTSAL 2019</h3>
+                <div className="row">
+                    <div className="col-xl-1">
+                        <Link className="button button-xs button-gray-outline" to="/">Home</Link>
+                    </div>
+                    <div className="col-xl-1">
+                        <Link className="button button-xs button-gray-outline" to="/results">Results</Link>
+                    </div>
                 </div>
-            </div>
-        </section>
 
-    </>
+            </div>
+        </div>
+    </section>
 )
 
 export default NavBar
